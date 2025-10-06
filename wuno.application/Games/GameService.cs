bbe67bdbee27aacae5ed41e4b14a58c5c8719d94
LSA125 <lsa125@sfu.ca>
2025-10-05 17:42:18 -0700

    Added datatypes to typescript and fixed errors in backend data.

--- wuno.application/Games/GameService.cs
+++ wuno.application/Games/GameService.cs
@@ -172,7 +172,7 @@ namespace Wuno.Application.Games
             game.NextSeat = seat;
             await Task.CompletedTask;
         }
-        async public Task<JoinGameRequest> JoinGameAsync(AppDbContext db, Guid gameId, Guid userId, CancellationToken ct)
+        async public Task<JoinGameResponse> JoinGameAsync(AppDbContext db, Guid gameId, Guid userId, CancellationToken ct)
         {
             var game = await db.Games
               .Include(g => g.Players)
@@ -190,7 +190,9 @@ namespace Wuno.Application.Games
             {
                 player.IsConnected = true;
                 await db.SaveChangesAsync(ct);
-                return new JoinGameRequest(gameId,);
+                GameState? gameState = await GetGameStateAsync(gameId, ct) as GameState;
+                if (gameState is null) throw new Exception("Failed to get game state after joining");
+                return new JoinGameResponse(gameId, gameState);
             }
             else if (game.Status != GameStatus.WAITING) throw new Exception("Game not joinable");
             var inactive = game.Players.FirstOrDefault(p => !p.IsActive);
@@ -199,7 +201,9 @@ namespace Wuno.Application.Games
             inactive.Name = user.Name;
             inactive.IconUrl = user.IconUrl;
             await db.SaveChangesAsync(ct);
-            return new JoinGameRequest(gameId);
+            GameState? state = await GetGameStateAsync(gameId, ct) as GameState;
+            if (state is null) throw new Exception("Failed to get game state after joining");
+            return new JoinGameResponse(gameId, state);
         }
         async public Task LeaveGameAsync(AppDbContext db, Guid gameId, Guid playerId, CancellationToken ct)
         {
